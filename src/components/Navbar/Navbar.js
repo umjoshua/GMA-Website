d2a0c5e222b67adf4d40f950e84ca4e2d9c83762
Antony Thomas <antonythomas993@gmail.com>
2023-06-09 16:02:45 +0530

    Navbar scroll color change added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,9 +5,18 @@ import CloseIcon from '@mui/icons-material/Close';
 
 function Navbar() {
   const [icon, setIcon] = useState(false);
+  const [color, setColor] = useState(false);
+  const changeColor = () => {
+    if(window.scrollY >= 100){
+      setColor(true);
+    }else{
+      setColor(false);
+    }
+  }
+  window.addEventListener("scroll",changeColor);
 
   return (
-    <div className="header">
+    <div className={color ? "header header_bg" : "header"}>
       <h1>Association</h1>
       <ul className={icon ? "nav_menu active" : "nav_menu"}>
         <li>Home</li>
