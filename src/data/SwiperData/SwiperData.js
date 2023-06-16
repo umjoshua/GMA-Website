cfc51f3fd982e00f7c48f7e7c9ef36b5449d240a
Antony Thomas <antonythomas993@gmail.com>
2023-06-16 11:38:26 +0530

    Swiper code updated

---
+++ src/data/SwiperData/SwiperData.js
@@ -0,0 +1,33 @@
+import Image1 from '../../assets/images/img_1.jpg'
+import Image2 from '../../assets/images/img_2.jpg' 
+import Image3 from '../../assets/images/img_3.jpg'
+import Image4 from '../../assets/images/img_4.jpg'
+import Image5 from '../../assets/images/img_5.jpg'
+import Image6 from '../../assets/images/img_6.jpg'
+import Image7 from '../../assets/images/img_7.jpg'
+
+const SwiperData = [
+    {
+        image: Image1
+    },
+    {
+        image: Image2
+    },
+    {
+        image: Image3
+    },
+    {
+        image: Image4
+    },
+    {
+        image: Image5
+    },
+    {
+        image: Image6
+    },
+    {
+        image: Image7
+    },
+]
+
+export default SwiperData;
